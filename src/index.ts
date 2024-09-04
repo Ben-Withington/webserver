d95dd0ca958e0391073801e15801cce8bdd98a3b
Ben Withington <180095385+Ben-Withington@users.noreply.github.com>
2024-09-04 12:31:11 +0100

    Basic echo server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-const test: string = "Get typescript working!";
+import * as net from "net"
 
-console.log(test);
+const newConnection = (socket: net.Socket): void => {
+    console.log("new connection", socket.remoteAddress, socket.remotePort);
+
+    socket.on('data', (data: Buffer) => {
+        console.log('data', data);
+        socket.write(data);
+
+        if(data.includes('q')) {
+            console.log("closing");
+            socket.end();
+        }
+    });
+
+    socket.on('end', () => {
+        console.log("EOF");
+    });
+}
+
+let server = net.createServer({allowHalfOpen: true});
+server.on('error', (err: Error) => { throw err; });
+server.on('connection', newConnection);
+server.listen({host: '127.0.0.1', port: 1234});
